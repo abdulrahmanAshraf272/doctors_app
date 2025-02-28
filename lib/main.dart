b110f31dad6bfd82e3b41b52ad9499b1a4e0a7d8
abdulrahmanAshraf272 <abdulrahmanashraf272@gmail.com>
2025-02-28 03:07:05 +0200

    - added splash screen fro IOS and Android below 12 - added splash screen for Android 12+

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,5 @@ import 'package:doctors_app/doc_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(DocApp(
-    appRouter: AppRouter(),
-  ));
+  runApp(DocApp(appRouter: AppRouter()));
 }
