635aa175e24d1869f3bf0a753229cd7a4f4bffdb
abdulrahmanAshraf272 <abdulrahmanashraf272@gmail.com>
2025-03-02 02:37:25 +0200

    - added di - added AppRegex - integrated with login EP - added login status with validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:doctors_app/core/di/dependency_injection.dart';
 import 'package:doctors_app/core/routing/app_router.dart';
 import 'package:doctors_app/doc_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  setupGetIt();
   runApp(DocApp(appRouter: AppRouter()));
 }
